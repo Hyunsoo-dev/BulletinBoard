9ddd52eb87b7637fc273468cfed10d262cb55192
Hyun Soo <jing07161@gmail.com>
2020-09-23 17:21:40 +0900

    User 컨트롤러 통합

--- BoardWeb/src/main/java/com/springbook/view/user/UserController.java
+++ BoardWeb/src/main/java/com/springbook/view/user/UserController.java
@@ -1,34 +1,32 @@
 package com.springbook.view.user;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
 
 import com.springbook.biz.user.UserVO;
 import com.springbook.biz.user.impl.UserDAO;
+import com.springbook.biz.user.impl.UserService;
 
 @Controller
-public class LoginController{
-	@RequestMapping(value="/login.do" , method=RequestMethod.GET)
-	public String loginView(UserVO vo) {
-		System.out.println("로그인 화면으로 이동");
-		vo.setId("test123");
-		vo.setPassword("test123");
-		return "login.jsp";
-	}
+public class UserController {
+	/*
+	 * User 컨트롤러 통합
+	*/
+	
+	@Autowired
+	private UserService userService;
 	
 	
+	//로그인 컨트롤러
 	@RequestMapping(value="/login.do" ,method=RequestMethod.POST)
 	public String login(UserVO vo,UserDAO userDAO,HttpSession session){
 		System.out.println("로그인 처리");
 		
-		UserVO user = userDAO.getUser(vo);
+		UserVO user = userService.getUser(vo);
 		
 		
 		if(user != null) {
@@ -39,5 +37,20 @@ public class LoginController{
 		}
 	
 	}
-
+	//로그아웃 컨트롤러 
+	@RequestMapping(value="/logout.do")
+	public String logout(HttpSession session){
+		
+		session.invalidate();
+		
+		
+		return "login.jsp";
+	}
+	
+	//회원가입 컨트롤러
+	@RequestMapping(value="join.do",method=RequestMethod.POST)
+	public String join(UserVO vo) {
+		userService.insertUser(vo);
+		return "login.jsp";
+	}
 }
